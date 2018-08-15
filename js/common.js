18bf13c4e81d40abe4133a1acb3ff2edb7814966
guojiabao <guojiabao@qq.com>
2018-08-15 18:34:53 +0800

    文件夹的新增与删除

--- js/common.js
+++ js/common.js
@@ -4,3 +4,21 @@ $(".open-modal").click(function(){
 $(".close-modal").click(function(){
   $(".mp-modal").fadeOut();
 });
+
+// 底部弹窗
+var footerPop={
+	show:function(){
+		$('.mp-popup').removeClass('active');
+		$('.mp-popmaskbg').remove();
+		$('.mp-popup_down').toggleClass('active');
+		if(!($('.mp-popmaskbg').length > 0)){
+			$('.mp-main').append("<div class='mp-popmaskbg'></div>");
+		}else{
+			$('.mp-popmaskbg').remove();
+		}
+	},
+	hide:function(){
+		$('.mp-popup').removeClass('active');
+		$('.mp-popmaskbg').remove();
+	}
+};

debf2952e1e40a629ee276a321eabb26ba8fd746
guojiabao <guojiabao@qq.com>
2018-08-26 19:19:57 +0800

    test

--- js/common.js
+++ js/common.js
@@ -78,6 +78,48 @@ function longtimetips(options){
   });
 }
 
+// 微站编辑组件确认提示
+/**
+ * 弹出但确定,取消按钮的提示
+ * @param {Object} content 设置弹出提示框的内容
+ * @param {Object} confirm 设置点击按钮的触发函数
+ * @param {Object} data  调用confirm时带过去的参数，confirm(data);
+ * @param {Object} value {'cancel':'取消','confirm':'确定'}
+ */
+function confirm(content,confirm,data,values){
+    var id = new Date().getTime();
+    this.values = values;
+    if(values == null){
+        this.values = {'cancel':'取消','confirm':'确定'};
+    }
+    var html = '';
+    if($(window).width() < 800){
+        html += '<div class="" style="box-shadow: 0 0 10px rgba(0,0,0,0.2);z-index: 20000; font-size: 1rem; border-radius: 8px; background: #f8f8f8; width: 80%;position: fixed; top: 50%; margin-top: -75px;  left: 50%;  margin-left: -40%;" id="j-toast-default'+id+'">';
+    }else{
+        html += '<div class="" style="box-shadow: 0 0 10px rgba(0,0,0,0.2);z-index: 20000; font-size: 1rem; border-radius: 8px; background: #f8f8f8; width:400px;position: fixed; top: 50%; margin-top: -75px;  left: 50%;  margin-left: -200px" id="j-toast-default'+id+'">';
+    }
+    html += '<div style="border-bottom:1px solid #e1e1e1;padding:10px;text-align:center;background-color#fff;color:#aaaaaa;">提示</div><div class="toast-cont'+id+'" style="line-height: 1.8;color:#4f4f4f;box-sizing:border-box;width:100%;  display: inline-block; padding: 20px 20px;text-align: center;">默认的Toast通知</div>';
+
+    html += '<div style="border-top: 1px solid #e1e1e1;"><input type="button" style="outline:0;width:50%;height:40px;color:#0079ff;text-align: center;padding: 12px 0;font-size: 16px;border-right:1px solid #e1e1e1;border-top:0;border-bottom:0;border-left:0;border-radius:8px 0 0 8px;background-color:#fff;" id="cancel'+id+'" value="'+this.values.cancel+'"><input type="button" id="confirm'+id+'" value="'+ this.values.confirm+'" style="outline:0;width:50%;height:40px;color:#0079ff;text-align: center;padding: 12px 0;font-size: 16px;border:none;border-radius:8px;background-color:#fff;"></div></div>';
+
+    html += '</div>';
+
+    html += '<div id="ShieldingLayer'+id+'" style="width:100%;height:100%;background: rgba(21, 20, 20, 0.3);z-index: 500;position: fixed;"></div>';
+    $('body').append(html);
+    $("#j-toast-default"+id+" .toast-cont"+id).html(content);
+    $('#cancel'+id).on('click',function(){
+        $("#j-toast-default"+id).remove();
+        $("#ShieldingLayer"+id).remove();
+    });
+    $('#confirm'+id).on('click',function(){
+        if(typeof confirm == 'function'){
+            confirm(data);
+            $("#j-toast-default"+id).remove();
+            $("#ShieldingLayer"+id).remove();
+        }
+    });
+}
+
 // 弹出输入框
 function popInputbox(options){
   var defaults = {
